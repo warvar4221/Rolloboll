bf9fd78506d6438a995528ff964be99aebb970be
Warvar <spasibo_t@mail.ru>
2025-05-21 21:04:38 +0300

    Get start

---
+++ My project/Assets/Skript/PlayerController.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 0;
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        winTextObject.SetActive(false);
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+    }
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    void SetCountText()
+    {
+            countText.text = "Count: " + count.ToString();
+        if (count >= 7)
+        {
+            winTextObject.SetActive(true);
+            countText.text = "";
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Destroy the current object
+            Destroy(gameObject);
+            // Update the winText to display "You Lose!"
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "Вы проиграли!";
+        }
+    }
+}
